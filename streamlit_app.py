1cf8205645cdd7e87cd4367f535f12448e9db94c
ispwells <ispwells@beechhallschool.org>
2024-09-18 14:25:42 +0100

    login started

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+import datetime
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+def login():
+    st.title("login")
+    st.write("this app allows you to add and remove users")
+    st.write("please try it out, remember to click Check User")
+    st.write("username = Dave")
+    st.write("password = p1")
+    enterusername = st.text_input("please enter username to update system")
+    enterpassword = st.text_input("please enter password", type="password")
+    if st.button("check user")
+login()
